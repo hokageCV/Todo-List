65565ccb374a9d216b902ff2f792f3e32959f3b9
hokageCV <chaitanyavaru@gmail.com>
2022-07-18 14:55:54 +0530

    local storage repeat rendering

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import { renderToDos } from "./newToDoDOM" ;
 import { addTask } from "./newToDoObject" ;
 import { initiate } from "./inititeLocalStorage";
 
 initiate();
-// renderToDos();
+
 
 document.getElementById('submit').addEventListener('click', addTask );
-// document.getElementById('submit').addEventListener('click', renderToDos );
--- src/inititeLocalStorage.js
+++ src/inititeLocalStorage.js
@@ -1,11 +1,14 @@
 function initiate(){
+    // pre-filled task
     let tasksKiList=[ {
-        task:"cleam-room",
-        desc:"clean-properly",
+        task:"cleam room",
+        desc:"thik se saaf kar",
         date:"2022-6-20",
         priority:"High",
         project: "",
     }];
+
+    // initiating local storage
     localStorage.setItem('AllTasksList', JSON.stringify(tasksKiList) );
 }
 
